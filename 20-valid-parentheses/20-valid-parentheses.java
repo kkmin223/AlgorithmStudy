ed777af47bd438236771934c9142dce2bbb24e91
Kim Kang Min <41673190+kkmin223@users.noreply.github.com>
2022-01-16 15:15:05 +0900

    Time: 3 ms (37.43%), Space: 38.8 MB (27.77%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char cur = s.charAt(i);
+            char top;
+            switch(cur){
+                case '(':
+                case '{':
+                case '[':
+                    st.push(cur);
+                    break;
+                case ')':
+                    if(st.isEmpty())
+                        return false;
+                    top = st.pop();
+                    if(top !='('){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(st.isEmpty())
+                        return false;
+                    top = st.pop();
+                    if(top !='{'){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(st.isEmpty())
+                        return false;
+                    top = st.pop();
+                    if(top !='['){
+                        return false;
+                    }
+                    break;
+            }
+        }
+        if(st.isEmpty()) return true;
+        else return false;
+    }
+}
