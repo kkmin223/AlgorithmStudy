8a6ae8bb5b9743a884cb82f095918a59c91cf8eb
Kim Kang Min <41673190+kkmin223@users.noreply.github.com>
2022-01-23 17:03:38 +0900

    Update 53-maximum-subarray.java

--- 03LeetCode/53-maximum-subarray/53-maximum-subarray.java
+++ 03LeetCode/53-maximum-subarray/53-maximum-subarray.java
@@ -28,3 +28,16 @@ class Solution {
         return max;
     }
 }
+
+//방법 2 리팩토링 Math.max -> 삼항 
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0]; // 최대값 저장
+        int sum = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            sum = (sum+nums[i] > nums[i])? sum+nums[i] : nums[i];
+            max = (sum > max) ? sum : max;
+        }
+        return max;
+    }
+}
