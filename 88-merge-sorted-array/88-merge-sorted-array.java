e1f8e52f017bf7925fbdb0e979ff3aa5d7493d58
Kim Kang Min <41673190+kkmin223@users.noreply.github.com>
2022-01-25 15:57:38 +0900

    Time: 0 ms (100.00%), Space: 39.5 MB (27.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,9 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int idx = m+n-1, idx1=m-1, idx2=n-1;
-        int[] temp = nums1;
         while(idx1 >= 0 && idx2 >= 0){
-            nums1[idx--] = (temp[idx1] > nums2[idx2])? temp[idx1--] : nums2[idx2--];
+            nums1[idx--] = (nums1[idx1] > nums2[idx2])? nums1[idx1--] : nums2[idx2--];
         }
         while(idx2 >= 0){
             nums1[idx--] = nums2[idx2--];
