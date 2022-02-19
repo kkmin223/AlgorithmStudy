d98a498c9da643b936e24631497f07fa78a7bf39
Kim Kang Min <rkdals133@gmail.com>
2022-02-19 21:47:55 +0900

    20220219

---
+++ 02프로그래머스/올바른괄호.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(')
+                st.push(c);
+            else {
+                if(st.isEmpty()) return false;
+                st.pop();
+            }
+        }
+        if(!st.isEmpty()) return false;
+        return true;
+    }
+}
