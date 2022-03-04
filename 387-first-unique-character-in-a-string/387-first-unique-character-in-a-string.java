6505dbd3ffc0a74a74e3a8a153fdb3b8852823a3
Kim Kang Min <rkdals133@gmail.com>
2022-03-04 21:54:20 +0900

    Time: 19 ms (65.18%), Space: 42.7 MB (81.59%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -3,7 +3,6 @@ class Solution {
         for(int i = 0; i<s.length(); i++){
             int first = s.indexOf(s.charAt(i));
             int last = s.lastIndexOf(s.charAt(i));
-            System.out.println(first + "   " + last);
             if(first == last) return i;
         }
         return -1;
