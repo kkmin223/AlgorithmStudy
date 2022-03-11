0185c7c945e8b340e3f7522658c87e1f8a3c282e
Kim Kang Min <rkdals133@gmail.com>
2022-03-11 22:41:33 +0900

    Time: 54 ms (57.99%), Space: 54.2 MB (32.93%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -2,9 +2,10 @@ class Solution {
     public String longestPalindrome(String s) {
         String answer = "";
         for(int i = 0; i<s.length(); i++){
+            // 길이가 홀수일 경우(가운데를 기준으로 같을때) ex) bab
             String temp = getPalindrome(s,i,i);
+            // 길이가 짝수일 경우 ex) bb
             String temp2 = getPalindrome(s,i,i+1);
-            System.out.println(temp+ " temp2 = "+temp2);
             temp = (temp.length() < temp2.length())? temp2 : temp;
             answer = (answer.length() < temp.length())? temp : answer;
         }
