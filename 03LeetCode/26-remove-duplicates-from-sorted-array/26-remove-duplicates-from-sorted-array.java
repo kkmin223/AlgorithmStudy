8a09b932ce953c6975f8c0f890ad7cda06415c6c
Kim Kang Min <rkdals133@gmail.com>
2022-03-25 21:42:13 +0900

    10주차 leetCode

--- 03LeetCode/26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 03LeetCode/26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0)
-            return 0;
-        int k = 0;
-        int duplicate = nums[0];
-        for(int i = 1; i<nums.length; i++){
-            if(nums[i] != duplicate){
-                nums[++k] = nums[i];
-                duplicate = nums[i];
+        int idx = 1;
+        for(int i = 0; i< nums.length-1; i++){
+            if(nums[i] != nums[i+1]){
+                nums[idx++] = nums[i+1];
             }
         }
-        return k+1;
+        return idx;
     }
 }
