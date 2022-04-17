60e36235b5eed20804afb513f5d7eb2fface9bf0
Kim Kang Min <rkdals133@gmail.com>
2022-04-17 17:44:05 +0900

    20220417

--- 02프로그래머스/표편집.java
+++ 02프로그래머스/표편집.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Stack;
 class Solution {
 
@@ -10,9 +9,9 @@ class Solution {
     }
     static Stack<Integer> deleteHistory = new Stack<>();
     static int cur;
-    static boolean[] isDeleted;
-    static int[] prev;
-    static int[] next;
+    static boolean[] isDeleted = new boolean[1000001];
+    static int[] prev = new int[1000001];
+    static int[] next = new int[1000001];
     static int max;
     public static void up(int x){
         for(int i=0; i<x; i++){
@@ -26,24 +25,22 @@ class Solution {
     }
     public static void delete(){
         isDeleted[cur] = true;
-        next[prev[cur]] = next[cur];
-        prev[next[cur]] = prev[cur];
+        if(prev[cur] != -1) next[prev[cur]] = next[cur];
+        if(next[cur] != max) prev[next[cur]] = prev[cur];
+        deleteHistory.push(cur);
         if(next[cur] == max) cur = prev[cur];
         else cur = next[cur];
-        deleteHistory.push(cur);
+
     }
     public static void revert(){
        int temp = deleteHistory.pop();
        isDeleted[temp] = false;
-       next[prev[temp]] = temp;
-       prev[next[temp]] = temp;
+       if(prev[temp] != -1) next[prev[temp]] = temp;
+       if(next[temp] != max) prev[next[temp]] = temp;
     }
 
     public static String solution(int n, int k, String[] cmd) {
         max = n;
-        prev = new int[n+1];
-        next = new int[n+1];
-        isDeleted = new boolean[n];
         for(int i = 0; i<n; i++){
             prev[i] = i-1;
             next[i] = i+1;
