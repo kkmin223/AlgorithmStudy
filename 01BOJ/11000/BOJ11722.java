0d0cf3857de7fe71e55ab0e0f5cedcd49aca1cd6
Kim Kang Min <rkdals133@gmail.com>
2022-04-27 15:22:11 +0900

    20220427

--- 01BOJ/11000/BOJ11722.java
+++ 01BOJ/11000/BOJ11722.java
@@ -9,17 +9,16 @@ public class BOJ11722 {
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         int[] dp = new int[n];
-        int max = 0;
+        int max = 1;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
         }
-        dp[0] = max;
         for(int i = 1; i<n; i++){
             for(int j = 0; j<i; j++){
                 if(arr[j] > arr[i]){
-                    dp[j] = Math.max(dp[j], dp[i]+1);
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
             max = Math.max(dp[i],max);
