5a809045d8e5c476c0dffea46ec6ea1d8cef6964
Kim Kang Min <rkdals133@gmail.com>
2022-05-11 12:22:52 +0900

    Time: 41 ms (5.23%), Space: 55 MB (5.33%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+class Solution {
+    List<String> answer = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        makeParentheses("", n, n, n*2);
+        return answer;
+    }
+    public void makeParentheses(String parenthes, int open, int close, int length){
+        if(parenthes.length() == length){
+            if(checkParentheses(parenthes)) answer.add(parenthes);
+            return;
+        }
+        makeParentheses(parenthes+"(", open-1, close, length);
+        makeParentheses(parenthes+")", open, close-1, length);
+    }
+    public boolean checkParentheses(String parenthes){
+        Stack<Character> st = new Stack<>();
+        for(char c : parenthes.toCharArray()){
+            if(c == '('){
+                st.push('(');
+            } else {
+                if(st.isEmpty()) return false;
+                st.pop();
+            }
+        }
+        if(!st.isEmpty()) return false;
+        return true;
+    }
+}
