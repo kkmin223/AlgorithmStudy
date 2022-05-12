1a7100e5cc7003b816e78c08b21fe77744a81e4f
Kim Kang Min <rkdals133@gmail.com>
2022-05-12 10:37:01 +0900

    20220512

--- 01BOJ/14000/BOJ14719.java
+++ 01BOJ/14000/BOJ14719.java
@@ -7,28 +7,27 @@ public class BOJ14719 {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         int w = sc.nextInt();
-        int[] block = new int[w];
+        boolean[][] block = new boolean[h][w];
+        int answer = 0;
         for(int i = 0; i<w; i++){
-            block[i] = sc.nextInt();
+            int cnt = sc.nextInt();
+            for(int j = 0; j<cnt; j++) block[j][i] = true;
         }
-        int idx1 = -1, idx2 =-1;
-        int sum = 0;
-        for(int i = 0; i<w; i++){
-            if(block[i] != 0 && idx1 == -1){
-                idx1 = i;
-                idx2 = i+1;
-            }
-            else if(block[i] != 0 && idx1 != -1 && block[i] > block[idx2]){
-                idx2 = i;
-            } else if(block[i] != 0 && idx1 != -1 && block[i] <= block[idx2] && i != idx2){
-                int min = Math.min(block[idx1], block[idx2]);
-                for(int j = idx1+1; j<idx2; j++){
-                    sum += block[j] - min;
+
+        for(int i = 0; i<h; i++){
+            int left = 0;
+            boolean flag = false;
+            for(int j = 0; j<w; j++){
+                if(block[i][j]){
+                    if(!flag){
+                        flag = true;
+                    } else {
+                        answer += j-left-1;
+                    }
+                    left = j;
                 }
-                idx1 = i;
-                idx2 = i+1;
             }
         }
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
