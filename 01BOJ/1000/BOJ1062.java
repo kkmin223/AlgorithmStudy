324387fc36b37a4125bfc57f454491e46c508977
Kim Kang Min <rkdals133@gmail.com>
2022-05-16 11:05:43 +0900

    20220516

--- 01BOJ/1000/BOJ1062.java
+++ 01BOJ/1000/BOJ1062.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ1062 {
-    static boolean[] alphabet = new boolean[26];
+    static boolean[] alphabet;
     static String[] words;
     static int answer = Integer.MIN_VALUE;
     static int k,n;
@@ -14,43 +14,59 @@ public class BOJ1062 {
         k = Integer.parseInt(s[1]);
         words = new String[n];
         for(int i = 0; i<n; i++){
-            words[i] = br.readLine();
+            String str = br.readLine();
+            //모든 단어가 anta와 tica를 포함하기 때문에 공백으로 바꿔서 처리
+            //필수로 배워야 하는 알파벳 a, c, i, n, t 가 있음
+            str = str.replace("anta", "");
+            str = str.replace("tica", "");
+            words[i] = str;
         }
-        if(k<5) System.out.println(0);
-        else {
-            alphabet[0] = true; //a
-            alphabet[2] = true; //c
-            alphabet[8] = true; // i
-            alphabet[13] = true; //n
-            alphabet[19] = true; //t
-            studyWord(5,k);
-            System.out.println(answer);
+        if(k<5) {
+            // 5개보다 적으면 a, c, i, n, t 를 배우지 못함
+            System.out.println(0);
+            return;
+        } else if(k==26) {
+            // 모든 알파벳을 다 배우는 경우
+            System.out.println(n);
+            return;
         }
-
+        alphabet = new boolean[26];
+        alphabet[0] = true; //a
+        alphabet[2] = true; //c
+        alphabet[8] = true; // i
+        alphabet[13] = true; //n
+        alphabet[19] = true; //t
+        studyWord(1,5);
+        System.out.println(answer);
     }
-    public static void studyWord(int alphabetCnt, int k){
+
+
+    public static void studyWord(int start, int alphabetCnt){
+        // 백트래킹을 이용해서 모든 경우의 수 탐색.
+        // 0~26으로 순차적으로 처리하기 때문에 start 매개변수로 이미 지나온 곳은 탐색하지 않게 해서 시간 복잡도를 줄임.
         if(alphabetCnt == k){
-            int wordCnt = 0;
+            int cnt = 0;
             for(int i = 0; i<n; i++){
                 boolean flag = true;
-                for(char c : words[i].substring(4, words[i].length()-4).toCharArray()){
-                    if(!alphabet[c-'a']){
+                for(int j = 0; j< words[i].length(); j++){
+                    // words[i]에 포함된 알파벳 중에 안배운 알파벳이 있는지 검사
+                    if(!alphabet[words[i].charAt(j) - 'a']){
                         flag = false;
                         break;
                     }
                 }
-                if(flag) wordCnt++;
+                if(flag) cnt++;
             }
-            answer = Math.max(wordCnt, answer);
-        } else {
-            for(String word: words){
-                for(char c : word.substring(4, word.length()-4).toCharArray()){
-                    if(alphabet[c-'a']) continue;
-                    alphabet[c - 'a'] = true;
-                    studyWord(alphabetCnt+1, k);
-                    alphabet[c-'a'] = false;
-                }
+            answer = Math.max(answer, cnt);
+            return;
+        }
+        for(int i = start; i<26; i++){
+            if(!alphabet[i]){
+                alphabet[i] = true;
+                studyWord(i,alphabetCnt+1);
+                alphabet[i] = false;
             }
         }
+
     }
 }
