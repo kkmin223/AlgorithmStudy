3caf773dbdfb24cafb8db7b3ce0bdbadb9d4a0a6
Kim Kang Min <rkdals133@gmail.com>
2022-05-20 16:52:53 +0900

    20220520

--- 01BOJ/1000/BOJ1916.java
+++ 01BOJ/1000/BOJ1916.java
@@ -1,25 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class BOJ1916 {
-    static class Bus{
-        int from;
-        int to;
-        int cost;
-        public Bus(int from, int to, int cost) {
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
+class Bus{
+    int from;
+    int to;
+    int cost;
+    public Bus(int from, int to, int cost) {
+        this.from = from;
+        this.to = to;
+        this.cost = cost;
     }
-    static Bus[] buses;
+}
+
+class Node implements Comparable<Node> {
+    int to;
+    int cost;
+
+    public Node(int to, int cost) {
+        this.to = to;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return cost - o.cost;
+    }
+}
+public class BOJ1916 {
+    static Bus[] buses; // edge 모음 - 벨맨포드에서 사용
     static int[] distance;
-    static int[][] map;
+    static ArrayList<ArrayList<Node>> list; // 인접리스트 - 다익스트라에서 사용
     static boolean[] visited;
     static int start, end;
     static int n,m;
@@ -28,7 +40,7 @@ public class BOJ1916 {
 //        bellmanFord(start);
         initD();
         dijkstra(start,end);
-        System.out.println(distance[end]);
+        System.out.println(dijkstra(start, end));
     }
     public static void initB() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,7 +61,7 @@ public class BOJ1916 {
         start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
     }
-    public static void bellmanFord(int start){
+    public static int bellmanFord(int start){
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[start] = 0;
         for(int i = 1; i<=n; i++){
@@ -61,47 +73,48 @@ public class BOJ1916 {
                 }
             }
         }
+        return distance[end];
     }
     public static void initD() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
         distance = new int[n+1];
-        map = new int[n + 1][n + 1];
-        visited = new boolean[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        list = new ArrayList<>();
+        for(int i = 0; i<=n; i++){
+            list.add(i, new ArrayList<>());
+        }
+
         for(int i = 0; i<m; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            map[from][to] = cost;
+            list.get(from).add(new Node(to, cost));
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
         start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
     }
-    public static void dijkstra(int start, int end){
-        Arrays.fill(distance, Integer.MAX_VALUE);
+    public static int dijkstra(int start, int end){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        visited = new boolean[n + 1];
         distance[start] = 0;
-        visited[start] = true;
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i<=n; i++){
-            if(map[start][i] == 0) continue;
-            q.offer(i);
-            distance[i] = map[start][i];
-        }
-        while(!q.isEmpty()){
-            int cur = q.poll();
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            int cur = node.to;
+            if(cur == end) break;
             if(visited[cur]) continue;
             visited[cur] = true;
-            for(int i = 0; i<=n; i++){
-                if(map[cur][i] == 0 || visited[i]) continue;
-                if(distance[i] >= distance[cur] + map[cur][i]){
-                    q.offer(i);
-                    distance[i] = distance[cur] + map[cur][i];
+            for (Node next : list.get(cur)) {
+                if(!visited[next.to] && distance[next.to] > distance[cur] + next.cost){
+                    distance[next.to] = distance[cur] + next.cost;
+                    pq.add(new Node(next.to, distance[next.to]));
                 }
             }
         }
-
+        return distance[end];
     }
 }
