d8a1141357525aa400af2ee34e65f569e36d0616
Kim Kang Min <rkdals133@gmail.com>
2022-05-31 16:06:42 +0900

    20220531

--- 01BOJ/17000/BOJ17070.java
+++ 01BOJ/17000/BOJ17070.java
@@ -7,23 +7,24 @@ import java.util.Queue;
 public class BOJ17070 {
     static int n;
     static int[][] home;
+    static int answer = 0;
     static class Pipe {
-        int a1;
-        int a2;
-        int b1;
-        int b2;
+        int y1;
+        int x1;
+        int y2;
+        int x2;
 
-        public Pipe(int a1, int a2, int b1, int b2) {
-            this.a1 = a1;
-            this.a2 = a2;
-            this.b1 = b1;
-            this.b2 = b2;
+        public Pipe(int y1, int x1, int b1, int b2) {
+            this.y1 = y1;
+            this.x1 = x1;
+            this.y2 = b1;
+            this.x2 = b2;
         }
 
         public int getStatus(){
             // 가로 1, 세로 2, 대각선 3
-            if(a1== b1) return 1;
-            else if(a2== b2) return 2;
+            if(y1 == y2) return 1;
+            else if(x1 == x2) return 2;
             else return 3;
         }
     }
@@ -38,77 +39,58 @@ public class BOJ17070 {
                 home[i][j] = Integer.parseInt(input[j]);
             }
         }
-        System.out.println(bfs(new Pipe(0,0,0,1)));
+        dfs(new Pipe(0, 0, 0, 1));
+        System.out.println(answer);
     }
-    public static int bfs(Pipe start){
-        int cnt = 0;
-        Queue<Pipe> q = new LinkedList<>();
-        q.add(start);
-        while(!q.isEmpty()){
-            Pipe cur = q.poll();
-            if(cur.b1 == n-1 && cur.b2 == n-1) {
-                cnt++;
-                continue;
+    public static void dfs(Pipe cur){
+        if(cur.y2 == n-1 && cur.x2 == n-1) {
+            answer++;
+            return;
+        }
+        int nx,ny;
+        int status = cur.getStatus();
+        if(status == 1){
+            ny = cur.y2;
+            nx = cur.x2 +1;
+            if(isIn(nx, ny)){
+                if(home[ny][nx] != 1){
+                    dfs(new Pipe(cur.y2, cur.x2, ny, nx));
+                }
             }
-            int nx,ny;
-            int status = cur.getStatus();
-            if(status == 1){
-                ny = cur.b1;
-                nx = cur.b2+1;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
+        } else if (status == 2){
+            ny = cur.y2 +1;
+            nx = cur.x2;
+            if(isIn(nx, ny)){
+                if(home[ny][nx] != 1){
+                    dfs(new Pipe(cur.y2, cur.x2, ny, nx));
                 }
-                ny = cur.b1+1;
-                nx = cur.b2+1;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx]!= 1 && home[ny-1][nx] != 1 && home[ny][nx-1] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
+            }
+        } else {
+            ny = cur.y2;
+            nx = cur.x2 +1;
+            if(isIn(nx, ny)){
+                if(home[ny][nx] != 1){
+                    dfs(new Pipe(cur.y2, cur.x2, ny, nx));
                 }
-            } else if (status == 2){
-                ny = cur.b1+1;
-                nx = cur.b2;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
-                }
-                ny = cur.b1+1;
-                nx = cur.b2+1;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx]!= 1 && home[ny-1][nx] != 1 && home[ny][nx-1] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
-                }
-            } else {
-                ny = cur.b1;
-                nx = cur.b2+1;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
-                }
-                ny = cur.b1+1;
-                nx = cur.b2;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
-                }
-                ny = cur.b1+1;
-                nx = cur.b2+1;
-                if(!isIn(nx,ny)){
-                    if(home[ny][nx]!= 1 && home[ny-1][nx] != 1 && home[ny][nx-1] != 1){
-                        q.add(new Pipe(cur.b1, cur.b2, ny, nx));
-                    }
+            }
+            ny = cur.y2 +1;
+            nx = cur.x2;
+            if(isIn(nx, ny)){
+                if(home[ny][nx] != 1){
+                    dfs(new Pipe(cur.y2, cur.x2, ny, nx));
                 }
             }
         }
-        return cnt;
+        ny = cur.y2 +1;
+        nx = cur.x2 +1;
+        if(isIn(nx, ny)){
+            if(home[ny][nx]!= 1 && home[ny-1][nx] != 1 && home[ny][nx-1] != 1){
+                dfs(new Pipe(cur.y2, cur.x2, ny, nx));
+            }
+        }
+
     }
     public static boolean isIn(int x, int y){
-        return x < 0 || x >=n || y < 0 || y >= n;
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
 }
