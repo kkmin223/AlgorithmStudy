28a293acbe6a25d3309621c7510519f1938bfedf
Kim Kang Min <rkdals133@gmail.com>
2022-06-12 13:56:15 +0900

    20220612

---
+++ 01BOJ/11000/BOJ11058.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ11058 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        int screen = 0;
+        int buffer = 0;
+        N = sc.nextInt();
+
+    }
+}
