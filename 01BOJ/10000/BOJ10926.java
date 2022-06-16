8a3c43f0e333837e1b525c69c517cfa5f29623f0
Kim Kang Min <rkdals133@gmail.com>
2022-06-16 13:14:09 +0900

    20220616

---
+++ 01BOJ/10000/BOJ10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        input += "??!";
+        System.out.println(input);
+    }
+}
