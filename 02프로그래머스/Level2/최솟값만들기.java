1a43fac7d1eef11a81407f7b49e44ef7d88ca1c6
Kim Kang Min <rkdals133@gmail.com>
2023-01-07 22:51:32 +0900

    20220107

---
+++ 02프로그래머스/Level2/최솟값만들기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int length = A.length;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = 0; i<length; i++){
+            answer += A[i] * B[length-i-1];
+        }
+
+        return answer;
+    }
+}
